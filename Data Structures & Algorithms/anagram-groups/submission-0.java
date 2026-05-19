21c83bdf015c5c438cd7c613c3783312cbc32d11
Devlin Saha <89380375+Delpha05@users.noreply.github.com>
2026-05-19 23:08:55 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+
+        for(String word : strs){
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+
+            String key = new String(ch);
+
+            map.putIfAbsent(key, new ArrayList<>());
+
+            map.get(key).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
